2cd5f6af0a5b2f1a3e25b112790aa2170acac5be
Sergii Paryzhskyi <sergii.paryzhskyi@holidaycheck.com>
2018-03-15 14:08:18 +0100

    Extract speed score from response

--- src/pageSpeed.js
+++ src/pageSpeed.js
@@ -1,5 +1,11 @@
+const R = require('ramda');
+
+const pickSpeedScore = R.path(['data', 'ruleGroups', 'SPEED', 'score']);
+
 module.exports = (fetcher) => {
     const urlToCheck = 'http://holidaycheck.de';
     const pageSpeedEndpoint = `https://www.googleapis.com/pagespeedonline/v4/runPagespeed?url=${urlToCheck}`;
-    fetcher(pageSpeedEndpoint);
+
+    return fetcher(pageSpeedEndpoint)
+        .then(pickSpeedScore);
 };
--- test/unit/src/pageSpeedSpec.js
+++ test/unit/src/pageSpeedSpec.js
@@ -3,10 +3,25 @@ import sinon from 'sinon';
 import pageSpeed from '../../../src/pageSpeed';
 
 test('fetches data from page speed online', t => {
-    const fetcher = sinon.stub();
+    const fetcher = sinon.stub().resolves();
     const expectedUrl = 'https://www.googleapis.com/pagespeedonline/v4/runPagespeed?url=http://holidaycheck.de';
     pageSpeed(fetcher);
 
     t.is(fetcher.callCount, 1);
     t.true(fetcher.calledWith(expectedUrl));
+});
+
+test('parses speed index from the response', async t => {
+    const score = 64;
+    const response = {
+        data: {
+            ruleGroups: {
+                SPEED: { score }
+            }
+        }
+    };
+    const fetcher = sinon.stub().resolves(response);
+    const result = await pageSpeed(fetcher);
+
+    t.is(result, score);
 });
